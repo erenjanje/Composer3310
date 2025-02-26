9a7666f3ed1b8fa3ec6803c8f36199528232b8b5
Erencan Ceyhan <erencan.ceyhan@hotmail.com>
2025-02-26 11:28:04 +0300

    Add arena
    Signed-off-by: Erencan Ceyhan <erencan.ceyhan@hotmail.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#include "havuz.h"
+
 int main(void) {
-    printf("Hello, World!\n");
+    Pool* const pool = havuz_create(0, heap_allocator);
+    int const* const x = havuz_new(pool, int, 31);
+    printf("%d\n", *x);
+    havuz_destroy(pool);
     return 0;
 }
